be7a25073b68b8b0c4fcbb7fff67f0301ec3611a
Muhammad Satrio Wibowo <str.wibowo98@gmail.com>
2019-03-02 11:11:14 +0700

    Add wrapper template

--- application/views/base_layout/sidebar.php
+++ application/views/base_layout/sidebar.php
@@ -1,4 +1,4 @@
-<div class="col-12 col-sm-4 col-md-3 p-0 sidebar-container">
+<div class="col-12 col-sm-4 col-md-2 p-0 sidebar-container">
   <nav>
     <div class="row no-gutters">
       <div class="sidebar">
@@ -21,30 +21,64 @@
                 </a>
               </ul>
             </div>
-            <li>
-              <ion-icon name="planet"></ion-icon><text>Enterprise &amp; Business Service</text>
-            </li>
-            <li>
-              <ion-icon name="planet"></ion-icon><text>Net &amp; IT Support</text>
-            </li>
-            <li>
-              <ion-icon name="home"></ion-icon><text>Customer Service - Non Marketing</text>
-            </li>
-            <li>
-              <ion-icon name="home"></ion-icon><text>Customer Service - Marketing</text>
-            </li>
-            <li>
-              <ion-icon name="home"></ion-icon><text>Finansial</text>
-            </li>
-            <li>
-              <ion-icon name="home"></ion-icon><text>Kepegawaian</text>
-            </li>
-            <li>
-              <ion-icon name="home"></ion-icon><text>Digital &amp; Strategic Portfolio</text>
-            </li>
-            <li>
-              <ion-icon name="home"></ion-icon><text>Wholesale &amp; International Service</text>
-            </li>
+            <a href="#">
+              <li>
+                <ion-icon name="business"></ion-icon><text>Business Service</text>
+              </li>
+            </a>
+            <a href="#">
+              <li>
+                <ion-icon name="git-network"></ion-icon><text>IT Support</text>
+              </li>
+            </a>
+            <a href="#csm_service" data-toggle="collapse">
+              <li>
+                <ion-icon name="people"></ion-icon><text>Customer Service</text>
+              </li>
+            </a>
+            <div id="csm_service" class="collapse tree-dropdown">
+              <ul>
+                <a href="">
+                  <li>
+                    <text>CSM Non-Marketing</text>
+                  </li>
+                </a>
+                <a href="">
+                  <li>
+                    <text>CSM Marketing</text>
+                  </li>
+                </a>
+              </ul>
+            </div>
+            <a href="#">
+              <li>
+                <ion-icon name="stats"></ion-icon><text>Finance</text>
+              </li>
+            </a>
+            <a href="#">
+              <li>
+                <ion-icon name="person-add"></ion-icon><text>Human Resource</text>
+              </li>
+            </a>
+            <a href="#">
+              <li>
+                <ion-icon name="git-branch"></ion-icon><text>Strategic Portfolio</text>
+              </li>
+            </a>
+            <a href="#who_service" data-toggle="collapse">
+              <li>
+                <ion-icon name="cube"></ion-icon><text>Wholesale</text>
+              </li>
+            </a>
+            <div id="who_service" class="collapse tree-dropdown">
+              <ul>
+                <a href="">
+                  <li>
+                    <text>Inventory</text>
+                  </li>
+                </a>
+              </ul>
+            </div>
           </ul>
         </div>
       </div>
