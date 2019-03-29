1e367d9268509c2ec257b237d88db20492ac071c
tengku m risky <tengku08@gmail.com>
2019-03-29 22:43:48 +0700

    modal

--- application/views/finance/index.php
+++ application/views/finance/index.php
@@ -35,7 +35,7 @@
                   <!-- Dibawah ini adalah template box yang kalian bisa pakai untuk pengembangan sistem -->
                   <!--tab pemasukkan-->
                 <div class="row">
-                  <div class="col-4">
+                  <div class="col-4" data-toggle="modal" data-target="#exampleModalCenter">
                     <div class="box">
                       <div class="box-body">
                         <div class="row">
@@ -107,5 +107,25 @@
       </div>
     </div>
     <?php $this->load->view('base_layout/js_mandatory')?>
+    <!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
   </body>
 </html>
