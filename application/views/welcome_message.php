da027a0d31edd56b2fd546461f7d96027f02440a
Fauzan Firdaus <faoezanfirdaoes@gmail.com>
2019-03-30 11:47:14 +0700

    gw edit db, config, tampilan

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,99 +1,121 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'hrm');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   
+   //if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      $query = mysqli_query($db,"SELECT COUNT(*) FROM pegawai");
+      //$jumBaris = (int)$query;
+      $sql = mysqli_query($db,"SELECT * FROM pegawai");
+      $datas = array();
+      while($data=mysqli_fetch_array($sql)){
+        $datas[] = $data;
+      }
+    
+   //}
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php $this->load->view('base_layout/head_tag'); ?>
+    <title>Employee Data</title>
+    <!-- Input CSS atau JS yang dibutuhkan setelah line ini -->
+    <!-- Taruh file css di folder /css-->
+    <!-- Taruh file js di /js-->
 
-	<style type="text/css">
+    <!-- Contoh cara input css, ganti sesuai kebutuhan -->
+    <?php /* echo link_tag('css/base_styles.css') */ ?>
+  </head>
+  <body>
+    <div class="container-fluid p-0">
+      <div class="row equal no-gutters">
+        <?php $this->load->view('base_layout/sidebar'); ?>
+        <div class="col-12 col-sm-8 col-md-9 col-xl-10 wrapper">
+          <?php $this->load->view('base_layout/topmenu'); ?>
+          <div class="col-12 wrapper-container">
+            <div class="main-wrapper">
+              <div class="container">
+                <div class="page-header">
+                  <!-- Silakan replace sesuai judul halaman divisi kalian -->
+                  <div class="page-title">
+                    Employee Data
+                  </div> <br>
+                  <!-- Subtitle opsional, tapi bila ingin diberi, jelaskan page anda dalam maks 8 kata -->
+                  <!--div class="page-subtitle">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </div-->
+                  <button type="button" class="btn" style="background-color: #999999" data-toggle="modal" data-target="#exampleModal">Add New Employee</button>
+                </div>
+                <div class="row">
+                  <!-- Silakan masukkan code tampilan divisi Anda di bagian ini. -->
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Full Name</th>
+                        <th scope="col">Gender</th>
+                        <th scope="col">E-mail</th>
+                        <th scope="col">Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      
+                        <?php
+                        
+                          foreach ($datas as $data) : ?>
+                            <tr>
+                            <th scope="row"> <?php echo $data['idpegawai'] ?> </th>
+                            <td> <?php echo $data['namapegawai'] ?> </td>
+                            <td> <?php echo $data['genderpegawai'] ?> </td>
+                            <td> <?php echo $data['emailpegawai'] ?> </td>
+                            <td> <?php echo $data['statuspegawai'] ?> </td>
+                            </tr>
+                          <?php 
+                          endforeach;
+                        ?>
+                      
+                    </tbody>
+                  </table>
+                </div>
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+                <div class="modal fade" id="exampleModal" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <form>
+                        <div class="form-group">
+                          <label for="recipient-name" class="col-form-label">Recipient:</label>
+                          <input type="text" class="form-control" id="recipient-name">
+                        </div>
+                        <div class="form-group">
+                          <label for="message-text" class="col-form-label">Message:</label>
+                          <textarea class="form-control" id="message-text"></textarea>
+                        </div>
+                      </form>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                      <button type="button" class="btn btn-primary">Add Employee</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-	body {
-		background-color: #fefefe;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	#container {
-		background: #fff;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
-
-<div id="hello" style="width: 100%;">
-	<div class="" style="text-align: center">
-		<img style="width: 20%;" src="https://cdn.dribbble.com/users/515550/screenshots/3558181/octopus.gif">
-	</div>
-</div>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
-</body>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php $this->load->view('base_layout/js_mandatory')?>
+  </body>
 </html>
