5dab0c7f34e66e57d53c0d121c19bc0572d2e111
tengku m risky <tengku08@gmail.com>
2019-04-19 12:53:05 +0700

    database

---
+++ database/finance.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 19 Apr 2019 pada 07.52
+-- Versi Server: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sisfo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `finance`
+--
+
+CREATE TABLE `finance` (
+  `id_pemasukan` int(11) NOT NULL,
+  `blanko` varchar(100) NOT NULL,
+  `tipe` varchar(4) NOT NULL,
+  `keperluan` varchar(200) NOT NULL,
+  `tanggal` date NOT NULL,
+  `debit` int(11) NOT NULL,
+  `kredit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `finance`
+--
+
+INSERT INTO `finance` (`id_pemasukan`, `blanko`, `tipe`, `keperluan`, `tanggal`, `debit`, `kredit`) VALUES
+(2, '2', 'K1', '1', '2019-04-12', 0, 1),
+(3, '1', 'M1', '1', '2019-04-26', 1, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `finance`
+--
+ALTER TABLE `finance`
+  ADD PRIMARY KEY (`id_pemasukan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `finance`
+--
+ALTER TABLE `finance`
+  MODIFY `id_pemasukan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
