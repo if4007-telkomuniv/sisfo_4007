91f22a62f1dcfacffb46aed29cc581bd525d57c3
tengku m risky <tengku08@gmail.com>
2019-04-20 16:50:40 +0700

    98

--- application/views/finance/index.php
+++ application/views/finance/index.php
@@ -182,7 +182,6 @@
         </div>
       </div>
     </div>
-<<<<<<< HEAD
   </div>
   <?php $this->load->view('base_layout/js_mandatory')?>
   <!-- Modal PEMASUKAN-->
@@ -319,35 +318,6 @@
           <button type="submit" class="btn btn-primary">Submit</button>
           </form>
         </div>
-=======
-    <?php $this->load->view('base_layout/js_mandatory')?>
-
-<!-- Modal Pemasukkan -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Input Pemasukkan</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="form-group">
-            <input type="number" class="form-control" id="#" placeholder="Nomor Blanko">
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" id="#" placeholder="Keterangan">
-          </div>
-          <div class="form-group">
-            <input type="date" class="form-control" id="#" placeholder="Tanggal" data-date-format="DD/MM/YYYY">
-          </div>
-          <div class="form-group">
-            <input type="number" class="form-control" id="#" placeholder="Nominal">
-          </div>
-        </form>
->>>>>>> 578ec9f2da91dab9b602255d739c7f12ab261b65
       </div>
     </div>
   </div>
@@ -366,7 +336,7 @@
         <div class="modal-body">
           <div class="form-group">
             <label for="tanggal">Tahun</label>
-            <input type="text" id="datepicker" class="form-control" placeholder="Tahun" name="tanggal" required>
+            <input type="number" id="datepicker" class="form-control" placeholder="Tahun" name="tanggal" required>
           </div>
 
         </div>
