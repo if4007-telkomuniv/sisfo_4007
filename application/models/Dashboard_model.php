7b9783602cb75d2fa28584d0ce66ca5c04dfa83a
Satrio Wibowo <str.wibowo98@gmail.com>
2019-05-02 00:05:11 +0700

    Fixed Dashboard cURL error

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -3,24 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard_model extends CI_Model {
   public function getAllSdm(){
-    return json_decode(
-    file_get_contents(API_BASE_URL."/dashboard/index_get?param=get_sdm"));
+    return json_decode($this->curl->simple_get(API_BASE_URL."/dashboard/index_get?param=get_sdm"));
    }
 
    public function getPemesanan(){
-    return json_decode(file_get_contents(API_BASE_URL."/dashboard/index_get?param=get_pemesanan&limit=5"));
+    return json_decode($this->curl->simple_get(API_BASE_URL."/dashboard/index_get?param=get_pemesanan&limit=5"));
 	}
 
   public function getTagihan(){
-    return json_decode(file_get_contents(API_BASE_URL."/dashboard/index_get?param=get_tagihan&limit=5"));
+    return json_decode($this->curl->simple_get(API_BASE_URL."/dashboard/index_get?param=get_tagihan&limit=5"));
   }
 
   public function getInventory(){
-    return json_decode(file_get_contents(API_BASE_URL."/dashboard/index_get?param=get_inventory&limit=5"));
+    return json_decode($this->curl->simple_get(API_BASE_URL."/dashboard/index_get?param=get_inventory&limit=5"));
   }
 
   public function getStatus(){
-    return json_decode(file_get_contents(API_BASE_URL."/dashboard/index_get?param=get_status_pemasangan&limit=5"));
+    return json_decode($this->curl->simple_get(API_BASE_URL."/dashboard/index_get?param=get_status_pemasangan&limit=5"));
   }
 
 }
